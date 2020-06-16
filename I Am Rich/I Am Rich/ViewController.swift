3252310b15c0d911748d8437652d233f63359f5d
Lawrence Dizon <lawdizon8@gmail.com>
2020-06-16 12:39:00 -0700

    final project files

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by user173146 on 5/31/20.
+//  Copyright © 2020 user173146. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
