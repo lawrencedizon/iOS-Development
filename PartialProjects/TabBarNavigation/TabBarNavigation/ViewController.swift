24e40fcfd4ea107c8414522394653ee1e42079b3
Lawrence Dizon <lawdizon8@gmail.com>
2020-07-05 11:57:45 -0700

    initial build

---
+++ PartialProjects/TabBarNavigation/TabBarNavigation/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  TabBarNavigation
+//
+//  Created by Lawrence Dizon on 7/5/20.
+//  Copyright © 2020 Lawrence Dizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+}
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+class ViewController4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+
+
