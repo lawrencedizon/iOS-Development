06196cc88fd51b6d8363154d1d0c5b060bbad7c2
Lawrence Dizon <lawdizon8@gmail.com>
2020-07-04 19:44:35 -0700

    initial base

---
+++ PartialProjects/LayoutPractice/LayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutPractice
+//
+//  Created by Lawrence Dizon on 7/4/20.
+//  Copyright © 2020 Lawrence Dizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
