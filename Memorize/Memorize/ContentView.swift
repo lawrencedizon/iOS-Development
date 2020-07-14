91f0afcd5333814327687b0ab935e27edc1aa0fd
Lawrence Dizon <lawdizon8@gmail.com>
2020-07-13 23:47:58 -0700

    initial build

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Lawrence Dizon on 7/13/20.
+//  Copyright © 2020 Lawrence Dizon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
