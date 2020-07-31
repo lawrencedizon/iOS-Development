c2d2c99ba20ee1a75966307e13873b97dabc5e8e
Lawrence Dizon <lawdizon8@gmail.com>
2020-07-31 11:17:39 -0700

    initial build

---
+++ ReadMe App/ReadMe App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadMe App
+//
+//  Created by Lawrence Dizon on 7/31/20.
+//  Copyright © 2020 Lawrence Dizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
