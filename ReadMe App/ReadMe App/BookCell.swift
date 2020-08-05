1e8fd16fd99992fde1a89637df12e9e244305dd0
Lawrence Dizon <lawdizon8@gmail.com>
2020-08-05 15:28:51 -0700

    created custom TavleView

---
+++ ReadMe App/ReadMe App/BookCell.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+class BookCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var authorLabel: UILabel!
+    @IBOutlet var reviewLabel: UILabel!
+        
+    @IBOutlet var readMeBookMark: UIImageView!
+    @IBOutlet var bookThumbnail: UIImageView!
+    
+    
+}
--- ReadMe App/ReadMe App/LibraryViewController.swift
+++ ReadMe App/ReadMe App/LibraryViewController.swift
@@ -15,7 +15,7 @@ class LibraryViewController: UITableViewController {
              fatalError("Nothing selected!")
             }
         
-        let book = Library.books[indexPath.row]
+        let book = Library.books[indexPath.row - 1]
         return DetailViewController(coder: coder, book: book)
         }
     override func viewDidLoad() {
@@ -23,18 +23,35 @@ class LibraryViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    // MARK:- Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Library.books.count
+        return Library.books.count + 1
     }
     
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell =  tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
+        if indexPath == IndexPath(row: 0, section: 0){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "NewBookCell", for: indexPath)
+            return cell
+        }
         
-        let book = Library.books[indexPath.row]
         
         
-        cell.textLabel?.text = book.title
-        cell.imageView?.image = book.image
+        
+        
+        guard let cell =  tableView.dequeueReusableCell(withIdentifier: "\(BookCell.self)", for: indexPath) as? BookCell
+            else {
+                fatalError("Could not create BookCell")
+        }
+        
+        
+        let book = Library.books[indexPath.row - 1]
+        
+        cell.titleLabel.text = book.title
+        cell.authorLabel.text = book.author
+        cell.bookThumbnail.image = book.image
+        cell.bookThumbnail.layer.cornerRadius = 12
         return cell
     }
 
