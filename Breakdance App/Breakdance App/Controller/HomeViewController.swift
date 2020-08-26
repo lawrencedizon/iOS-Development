0bbe2797251e3729f6e4215ea87deb14c901550a
Lawrence Dizon <lawdizon8@gmail.com>
2020-08-26 12:11:18 -0700

    replaced Home tab with UIViewController instead of UITableViewController

---
+++ Breakdance App/Breakdance App/Controller/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Breakdance App
+//
+//  Created by Lawrence Dizon on 8/11/20.
+//  Copyright © 2020 Lawrence Dizon. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+}
+
--- Breakdance App/Breakdance App/Model/Profiles.swift
+++ Breakdance App/Breakdance App/Model/Profiles.swift
@@ -4,8 +4,19 @@ import UIKit
 
 enum Profiles {
     static let profiles: [Profile] = [
-        Profile(name: "Hong 10", dateOfBirth: Date(), Country: "South Korea"),
-        Profile(name: "Thesis", dateOfBirth: Date(), Country: "USA"),
-        Profile(name: "Cloud", dateOfBirth: Date(), Country: "USA")
+        Profile(name: "Omar", dateOfBirth: Date(), Country: "South Korea"),
+        Profile(name: "Lilou", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Hong 10", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Ronnie", dateOfBirth: Date(), Country: "South Korea"),
+        Profile(name: "Wing", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Neguin", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Roxrite", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Mounir", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Menno", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Viktor", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Issei", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Ami", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Lil Zoo", dateOfBirth: Date(), Country: "USA"),
+        Profile(name: "Kastet", dateOfBirth: Date(), Country: "USA"),
     ]
 }
