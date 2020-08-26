68e6dcc043c72f10d42bd40fb4d9f2aed185b01e
Lawrence Dizon <lawdizon8@gmail.com>
2020-08-26 12:44:08 -0700

    Setup new TableView and constraints

--- Breakdance App/Breakdance App/Controller/HomeViewController.swift
+++ Breakdance App/Breakdance App/Controller/HomeViewController.swift
@@ -1,14 +1,36 @@
-//
-//  ViewController.swift
-//  Breakdance App
-//
-//  Created by Lawrence Dizon on 8/11/20.
-//  Copyright © 2020 Lawrence Dizon. All rights reserved.
-//
-
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = UIColor.white
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+//MARK:- Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+    }
+}
+
+//MARK:- HomeViewController Functions
+
+extension HomeViewController {
+    //Setup TableView and add constraints
+    func setupTableView(){
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+        ])
+    }
     
 }
 
