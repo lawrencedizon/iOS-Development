940b3127bc91f5edd5fb535684d52c53110ee8af
Lawrence Dizon <lawdizon8@gmail.com>
2020-08-27 13:26:16 -0700

    completed TableViewCell setup and data

--- Breakdance App/Breakdance App/Controller/HomeViewController.swift
+++ Breakdance App/Breakdance App/Controller/HomeViewController.swift
@@ -1,10 +1,25 @@
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource   {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! ThirtyDayCell
+        
+        cell.backgroundColor = UIColor.white
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
     
     let tableView: UITableView = {
         let tv = UITableView()
-        tv.backgroundColor = UIColor.white
         tv.translatesAutoresizingMaskIntoConstraints = false
         return tv
     }()
@@ -21,8 +36,13 @@ class HomeViewController: UIViewController {
 extension HomeViewController {
     //Setup TableView and add constraints
     func setupTableView(){
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(ThirtyDayCell.self, forCellReuseIdentifier: "cellId")
         view.addSubview(tableView)
+    
+        
         
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
