2a8153787d334d0040cb568a233b5bd7534bf174
Lawrence Dizon <lawdizon8@gmail.com>
2020-10-12 08:32:45 -0700

    reset HomeView Controller

--- Breakdance App/Breakdance App/Controller/HomeViewController.swift
+++ Breakdance App/Breakdance App/Controller/HomeViewController.swift
@@ -1,56 +1,9 @@
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource   {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
+class HomeViewController: UIViewController{
+
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! ThirtyDayCell
-        
-        cell.backgroundColor = UIColor.white
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
-    
-    
-    let tableView: UITableView = {
-        let tv = UITableView()
-        tv.translatesAutoresizingMaskIntoConstraints = false
-        return tv
-    }()
-    
-//MARK:- Life Cycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupTableView()
-    }
 }
 
-//MARK:- HomeViewController Functions
 
-extension HomeViewController {
-    //Setup TableView and add constraints
-    func setupTableView(){
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        tableView.register(ThirtyDayCell.self, forCellReuseIdentifier: "cellId")
-        view.addSubview(tableView)
-    
-        
-        
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
-            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-            tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
-            tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
-        ])
-    }
-    
-}
 
