f1a04082d87b1dbd3137de6a838948f39d783eca
ldizon8 <lawdizon8@gmail.com>
2021-01-21 18:44:34 -0800

    Initial commit

---
+++ Mood/Mood/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Mood
+//
+//  Created by Lawrence Dizon on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
