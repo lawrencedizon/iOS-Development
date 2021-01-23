d4c395d8f839e37701a23172d1fb3d9788bdc6c0
ldizon8 <lawdizon8@gmail.com>
2021-01-22 19:32:24 -0800

    Complete Lesson 4 Challenge

--- VerySimpleSwiftUI/VerySimpleSwiftUI/ContentView.swift
+++ VerySimpleSwiftUI/VerySimpleSwiftUI/ContentView.swift
@@ -9,33 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Spacer()
+        VStack{
             
-            Image("logo")
+            // CN Tower Card
+            ZStack {
+                Image("toronto")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                
+                VStack {
+                    Text("CN Tower")
+                        .font(.largeTitle)
+                    
+                    Text("Toronto")
+                        .font(.footnote)
+                       
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.white)
+                .background(Color.black)
+                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                .cornerRadius(10)
+            }
+            .padding()
+            
+            //Big Ben Card
+            ZStack {
+                Image("london")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-            
-            Spacer()
-            HStack() {
-                Spacer()
-                Text("Hello!")
-                Spacer()
-                Text("World!")
-                Spacer()
+                .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                
+            VStack {
+                Text("Big Ben")
+                    .font(.largeTitle)
+                   
+                Text("London")
+                    .font(.footnote)
+                
             }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .foregroundColor(Color.white)
+            .background(Color.black)
+            .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+            .cornerRadius(10)
+                
+            }
+            .padding()
             
-            Spacer()
         }
-
-      
-            
-            
+  
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            
+        }
     }
 }
