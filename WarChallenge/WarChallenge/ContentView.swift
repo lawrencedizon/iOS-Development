384300f2a06daa3194aa310dc0e9e2370f210c12
ldizon8 <lawdizon8@gmail.com>
2021-01-22 20:32:02 -0800

    Complete UI construction

---
+++ WarChallenge/WarChallenge/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  WarChallenge
+//
+//  Created by Lawrence Dizon on 1/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Image("background")
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }.foregroundColor(.white)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WarChallenge/WarChallenge/WarChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarChallengeApp.swift
+//  WarChallenge
+//
+//  Created by Lawrence Dizon on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct WarChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
