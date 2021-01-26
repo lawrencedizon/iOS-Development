d4dd378089afdda180399be3d33d7a3200de0720
ldizon8 <lawdizon8@gmail.com>
2021-01-25 18:08:42 -0800

    Complete programmatic TableView code

--- iOSAcademy/ProgrammaticTableViews/ProgrammaticTableViews/ViewController.swift
+++ iOSAcademy/ProgrammaticTableViews/ProgrammaticTableViews/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var data = [String]()
+    
 
+    
+
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for x in 0...100 {
+            data.append("Some Data \(x)")
+        }
+        
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
     }
 
+    // Functions required for TableView Data Source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+         
+        cell.textLabel?.text = data[indexPath.row] 
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("cell tapped")
+    }
 
 }
 
