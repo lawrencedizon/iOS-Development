b0a4de07b773e8ea0d866902a8ba6209e18c1706
ldizon8 <lawdizon8@gmail.com>
2021-01-25 18:26:39 -0800

    Initial commit

---
+++ iOSAcademy/RingAnimation/RingAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RingAnimation
+//
+//  Created by Lawrence Dizon on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
