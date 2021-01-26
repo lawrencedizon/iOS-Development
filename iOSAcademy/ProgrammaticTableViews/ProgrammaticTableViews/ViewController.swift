e8852f63464f9c3d734805db938e0bf65c6a2757
ldizon8 <lawdizon8@gmail.com>
2021-01-25 17:41:50 -0800

    Initial commit

---
+++ iOSAcademy/ProgrammaticTableViews/ProgrammaticTableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticTableViews
+//
+//  Created by Lawrence Dizon on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
