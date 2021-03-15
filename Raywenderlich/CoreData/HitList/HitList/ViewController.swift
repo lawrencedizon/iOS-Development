047c09c964cbbadd5d5767c7c378965b2bd824d9
ldizon8 <lawdizon8@gmail.com>
2021-03-15 15:34:06 -0700

    Chapter 1 Complete: Save & fetching

--- Raywenderlich/CoreData/HitList/HitList/ViewController.swift
+++ Raywenderlich/CoreData/HitList/HitList/ViewController.swift
@@ -15,10 +15,34 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK - ViewController Lifecycle States
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "The List"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        
+        // 1
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // 2
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        // 3
+        
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        }catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
     }
 
     @IBAction func addName(_ sender: UIBarButtonItem) {
