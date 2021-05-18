f87cd008cc69acb9796f4c0320253d6faa100aa4
ldizon8 <lawdizon8@gmail.com>
2021-05-18 12:24:34 -0700

    Complete CodeCoverage example

---
+++ Testing/Unit Testing/CodeCoverage/CodeCoverage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeCoverage
+//
+//  Created by Lawrence Dizon on 5/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
